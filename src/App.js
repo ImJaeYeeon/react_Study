1925574c29b52dc8bae6c1a9433057e371642448
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-21 23:30:05 +0900

    react 사용법2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div>
-      Hello <b>react</b>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     Hello <b>react</b>
+  //   </div>
+  // );
+  return React.createElement("div",null,"Hello",React.createElement("b",null,"react"));
 }
 
 export default App;

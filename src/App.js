0205dcc61100c4437f12231465aa57c41e29fbdc
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 22:46:15 +0900

    감싸인 요소1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function App() {
+//function App() {
   // return (
   //   <div>
   //     Hello <b>react</b>
   //   </div>
   // );
-  return React.createElement("div",null,"Hello",React.createElement("b",null,"react"));
-}
+  // return React.createElement("div",null,"Hello",React.createElement("b",null,"react"));
+  function App(){
+    return(
+    <div>{/*div로 감싸야 실행됨*/}
+      <h1>리액트 안녕</h1>
+      <h2>잘 작동 하니?</h2>
+    </div>
+    );
+  }
 
 export default App;

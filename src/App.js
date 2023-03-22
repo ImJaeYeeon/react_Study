9c4416d02de78d3c265fa213c3cd8c29495a0105
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 23:47:54 +0900

    인라인 스타일2

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import React, { Fragment } from 'react';
 
 function App(){
   const name = '리액트';
-  const style1 = {
-    //background-color는 backgroundColor와 값이 -가 사라지고 카멜 표기법으로 작성
-    backgroundColor : 'black',
-    color : 'aqua',
-    fontSize : '48px', //font-size --> fontSize
-    fontWeight : 'bold',
-    padding : 16 //단위를 생략하면 px로 지정됨
-  };
-  return <div style={style1}>{name}</div>
+  return(
+  <div
+    style={{
+      //background-color 는 backgroundColor와 값이 -가 사라지고 카멜 표기법으로 작성
+      backgroundColor : 'black',
+      color : 'aqua',
+      fontSize : '48px', //font-size --> fontSize
+      fontWeight : 'bold',
+      padding : 16 //단위를 생략하면 px로 지정된다.
+    }}
+    >
+      {name}
+    </div>
+  )
 }
 
 export default App;

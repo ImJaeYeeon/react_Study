17aca467be2fea4ebfa5b70a7f3a33723f4fc520
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 22:50:24 +0900

    감싸인 요소2

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ import React, { Fragment } from 'react';
   // return React.createElement("div",null,"Hello",React.createElement("b",null,"react"));
   function App(){
     return(
-    <Fragment>{/*태그로(div, fragment )감싸야 실행됨*/}
+    <>{/*감싸야 실행됨*/}
       <h1>리액트 안녕</h1>
       <h2>잘 작동 하니?</h2>
-    </Fragment>
+    </>
     );
   }
 

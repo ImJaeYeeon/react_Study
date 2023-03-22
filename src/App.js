8d1643c8170eab5df693c1e32c6013c18e5c9ae7
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 23:30:10 +0900

    undefined

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import React, { Fragment } from 'react';
 
 function App(){
-  const name = '리액트';
-  const number = 0;
-  //return <div>{name === '리액트'&&<h1>리액트입니다.</h1>}</div>
-  return number && <div>내용</div>
+  const name = undefined;
+  //return name ; //안나옴
+  // return name || '값이 undefined입니다요' //후자 값 나옴
+  // return <div>{name}</div>; //안나옴
+  return <div>{name || '리액트'}</div>; //후자 값 나옴
 }
 
 export default App;

0e08b084919ca78f7b04b19cea7eb7dd4d36a40a
ImJaeYeeon <w2109@e-mirim.hs.kr>
2023-03-22 22:53:03 +0900

    자바스크립트 표현

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Fragment } from 'react';
 
-//function App() {
-  // return (
-  //   <div>
-  //     Hello <b>react</b>
-  //   </div>
-  // );
-  // return React.createElement("div",null,"Hello",React.createElement("b",null,"react"));
-  function App(){
-    return(
-    <>{/*감싸야 실행됨*/}
-      <h1>리액트 안녕</h1>
-      <h2>잘 작동 하니?</h2>
+function App(){
+  const name = '리액트';
+  return(
+    <>
+    <h1>{name} 안녕 </h1>
+    <h2>잘 작동 하니?</h2>
     </>
-    );
-  }
+  );
+}
 
 export default App;
